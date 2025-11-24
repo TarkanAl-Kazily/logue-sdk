f6fcb1b6c1c39453d88edb0037cf23fef9a6330f
Tarkan Al-Kazily <tarkan.alkazily@gmail.com>
2025-11-23 16:08:51 -0800

    [blit-osc] Manually formatting header.c

--- platform/nts-1_mkii/blit-osc/header.c
+++ platform/nts-1_mkii/blit-osc/header.c
@@ -74,15 +74,8 @@ const __unit_header unit_header_t unit_header = {
          {0, 1023, 0, 0, k_unit_param_type_none, 0, 0, 0, {"ALT"}},
 
          // 8 Edit menu parameters
-         {0,
-          3,
-          0,
-          1,
-          k_unit_param_type_strings,
-          0,
-          0,
-          0,
-          {"PARAM3"}},  // Example of a strings type parameter
+         // Example of a strings type parameter
+         {0, 3, 0, 1, k_unit_param_type_strings, 0, 0, 0, {"PARAM3"}},
          {0, 0, 0, 0, k_unit_param_type_none, 0, 0, 0, {""}},
          {0, 0, 0, 0, k_unit_param_type_none, 0, 0, 0, {""}},
          {0, 0, 0, 0, k_unit_param_type_none, 0, 0, 0, {""}},
